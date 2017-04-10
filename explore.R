760a06e15bbd20d58c133d12f0bd33874c519548
Jason Freeberg <jr.freeberg@comcast.net>
2017-04-10 15:23:06 -0700

    meeting 1

--- explore.R
+++ explore.R
@@ -1,10 +1,16 @@
 
 ### Master file for Exploratory Analysis
 
+library(ggplot2)
+library(plyr)
+library(dplyr)
+
+transitioned <- read.csv(file="transitioned.csv", header=T)
+censored <- read.csv(file="censored.csv", header=T)
+full <- rbind(transitioned, censored)
 
 # Example:
 # source("someRFile.R")
 # This script (explore.R) will then run everything in someRFile.R
-
 
 
