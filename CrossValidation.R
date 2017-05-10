d42b116345ccb3fa98dfebdd87e825aa8f282658
ziyijiang <ziyijiang@umail.ucsb.edu>
2017-05-10 14:55:11 -0700

    5/10
    Try predicting the duration

--- CrossValidation.R
+++ CrossValidation.R
@@ -12,20 +12,29 @@ test <- full[-trainIndx, ]
 # Double check that train+test == full
 assertthat::are_equal((nrow(train) + nrow(test)), nrow(full) )
 
-# Make two survival objects
-# trainSurv <- Surv(train$DURATION, train$event)
-# testSurv <- Surv(test$DURATION, test$event)
-
 # Remake models on train
 reducModel <- coxph(Surv(DURATION,event) ~ AGE_PRE + BMInew + SC, data = train)
 fullModel  <- coxph(Surv(DURATION,event) ~ BMInew + AGE_PRE + sex + cigar+ alcohol + Socio.Economic, data = train)
 
-cox.zph(reducModel)
-cox.zph(fullModel)
+# Predict on the testing sets with both models
+test$pred1 <- exp(predict(reducModel, newdata = test, type = "lp"))
+test$pred2 <- exp(predict(fullModel, newdata = test, type = "lp"))
 
-summary(test$AGE_PRE)
+base1 <- basehaz(reducModel)
+base2 <- basehaz(fullModel)
 
-pred1 <- predict(reducModel, newdata = test, type = "lp")
-pred2 <- predict(fullModel, newdata = test, type = "expected")
+r <- 1 - sum(train$event)/nrow(train)
+r <- .5
+r1 <- mean(exp(-predict(reducModel, type = "expected")))
+r2 <- mean(exp(-predict(fullModel, type = "expected")))
 
-prob1 <- exp(-pred1)
+
+for (i in 1:nrow(test)){
+  test$time.pred1[i] <- base1[base1$hazard >= 0.9/test$pred1[i], 2][1]
+  test$time.pred2[i] <- base2[base2$hazard >= 0.9/test$pred2[i], 2][1]
+}
+
+mae1 <- mean(abs(test$time.pred1-test$DURATION))
+mae2 <- mean(abs(test$time.pred2-test$DURATION))
+
+mean(full$DURATION)
